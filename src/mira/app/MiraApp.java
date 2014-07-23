918142d92cfa5a0a0fc86b5eaaaa3368708b2e77
codeanticode <andres.colubri@gmail.com>
2014-07-23 14:56:30 -0400

    integrated login UI

--- src/mira/app/MiraApp.java
+++ src/mira/app/MiraApp.java
@@ -2,11 +2,23 @@
 
 package mira.app;
 
+import java.awt.Desktop;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 import lib.math.SoftFloat;
 import lib.ui.Interface;
@@ -65,6 +77,10 @@ public class MiraApp extends PApplet {
   protected float animTime;
   protected SoftFloat animAlpha;  
  
+  protected String username;
+  protected String password;
+  protected boolean user_authenticated = false;
+  
   public int sketchQuality() {
     return SMOOTH_LEVEL;
   }
@@ -226,9 +242,95 @@ public class MiraApp extends PApplet {
                  "outputSelected", file, new ProfileHandler(vars));
   }
   
-  public void uploadSession() {
+  public void exportSelection() {
     
   }
+  
+  public void uploadSession() {
+    JFrame frame = new JFrame("SEErador - Login");
+    frame.setSize(300, 150);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JPanel panel = new JPanel();
+    frame.add(panel);
+    placeComponents(panel);
+
+    frame.setVisible(true);    
+    
+    
+  }
+  
+  
+  protected void placeComponents(JPanel panel) {
+    panel.setLayout(null);
+
+    JLabel userLabel = new JLabel("User");
+    userLabel.setBounds(10, 10, 80, 25);
+    panel.add(userLabel);
+
+    final JTextField userText = new JTextField(20);
+    userText.setBounds(100, 10, 160, 25);
+    panel.add(userText);
+
+    JLabel passwordLabel = new JLabel("Password");
+    passwordLabel.setBounds(10, 40, 80, 25);
+    panel.add(passwordLabel);
+
+    final JPasswordField passwordText = new JPasswordField(20);
+    passwordText.setBounds(100, 40, 160, 25);
+    panel.add(passwordText);
+
+    JButton loginButton = new JButton("Login");
+    loginButton.setBounds(5, 80, 80, 25);
+    panel.add(loginButton);
+    
+    JButton registerButton = new JButton("What's this?");
+    registerButton.setBounds(160, 80, 100, 25);
+    panel.add(registerButton);
+    
+    loginButton.addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent event){
+          
+          username = userText.getText();
+          password = passwordText.getText();
+          
+//          HttpClientExample client;
+          try {
+          user_authenticated = HttpClientExample.authenticate(username,password);
+          System.out.println(user_authenticated);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+          
+          //(new Thread(mirac)).start();
+        
+        }
+        
+      });
+    registerButton.addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent event){
+          
+          try {
+
+             String url ="http://localhost/classes/access_user/register.php";
+
+             Desktop dt = Desktop.getDesktop();
+             URI uri = new URI(url);
+             dt.browse(uri.resolve(uri));
+
+
+         } catch (URISyntaxException ex) {
+         } catch (IOException ex) {
+         }
+        
+        }
+        
+      });
+    
+    
+  } 
   
   //////////////////////////////////////////////////////////////////////////////
   
