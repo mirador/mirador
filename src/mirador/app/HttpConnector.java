73776ddd6da15008fc31d77ea78360ef36c8fe9f
codeanticode <andres.colubri@gmail.com>
2014-07-28 17:55:57 -0400

    Some code formatting

--- src/mirador/app/HttpConnector.java
+++ src/mirador/app/HttpConnector.java
@@ -22,222 +22,199 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
- 
+
 public class HttpConnector {
- 
   private String cookies;
   static private HttpClient client = HttpClientBuilder.create().build();
   //HttpClient client = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
-  
-  
   //private DefaultHttpClient client = new DefaultHttpClient();
   //client.setRedirectStrategy(new LaxRedirectStrategy());
 
   private final String USER_AGENT = "Mozilla/5.0";
- 
-  
-  
+
   public HttpConnector(){
-	  
+
   }
-  
 
-	public static void upload(String username, String password, String url,String db, String var1, String var2, String ranges, String historystring) throws ConnectException, Exception{
-		CookieHandler.setDefault(new CookieManager());
-		 
-		HttpConnector http = new HttpConnector();
-		
-	 
-		String result = http.GetPageContent(url);
-		System.out.println(result);
-	 
-		System.out.println("Got the add submission page. Let's parse.");
-		
-		client = HttpClientBuilder.create().build();
-		
-		List<NameValuePair> submissionParams = 
-	            http.getFormParams(result,username,password, db, var1, var2, ranges,historystring);
-		http.sendPost(url, submissionParams);
-		
-		
-	}
+  public static void upload(String username, String password, String url,String db, String var1, String var2, String ranges, String historystring) throws ConnectException, Exception{
+    CookieHandler.setDefault(new CookieManager());
 
-	public static boolean authenticate(String username, String password) throws Exception, ConnectException{
+    HttpConnector http = new HttpConnector();
 
-		String url = "http://localhost/classes/access_user/login.php";
-		CookieHandler.setDefault(new CookieManager());
-		HttpConnector http = new HttpConnector();
-		String page = http.GetPageContent(url);
-		List<NameValuePair> postParams = 
-	               http.getFormParams(page, username, password);
-		
-		boolean authenticated = http.sendPost(url, postParams);
-		
-		//System.out.println(authenticated);
-		return authenticated;
-		
-		
-	}
+    String result = http.GetPageContent(url);
+    System.out.println(result);
 
- 
+    System.out.println("Got the add submission page. Let's parse.");
+
+    client = HttpClientBuilder.create().build();
+
+    List<NameValuePair> submissionParams = 
+        http.getFormParams(result,username,password, db, var1, var2, ranges,historystring);
+    http.sendPost(url, submissionParams);
+  }
+
+  public static boolean authenticate(String username, String password) throws Exception, ConnectException{
+    String url = "http://localhost/classes/access_user/login.php";
+    CookieHandler.setDefault(new CookieManager());
+    HttpConnector http = new HttpConnector();
+    String page = http.GetPageContent(url);
+    List<NameValuePair> postParams = 
+        http.getFormParams(page, username, password);
+
+    boolean authenticated = http.sendPost(url, postParams);
+
+    //System.out.println(authenticated);
+    return authenticated;
+  }
+
   private boolean sendPost(String url, List<NameValuePair> postParams) 
-        throws Exception, HttpHostConnectException{
+      throws Exception, HttpHostConnectException{
+    HttpPost post = new HttpPost(url);
 
- 
-	HttpPost post = new HttpPost(url);
- 
-	// add header
-	post.setHeader("Host", "localhost");
-	post.setHeader("User-Agent", USER_AGENT);
-	post.setHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-	post.setHeader("Cookie", getCookies());
-	post.setHeader("Connection", "keep-alive");
-	post.setHeader("Referer", "http://localhost/classes/access_user/login.php");
-	post.setHeader("Content-Type", "application/x-www-form-urlencoded");
- 
-	post.setEntity(new UrlEncodedFormEntity(postParams));
- 
-	HttpResponse response = client.execute(post);
- 
-	int responseCode = response.getStatusLine().getStatusCode();
-	String reasonPhrase = response.getStatusLine().getReasonPhrase();
- 
-	System.out.println("\nSending 'POST' request to URL : " + url);
-	System.out.println("Post parameters : " + postParams);
-	System.out.println("Response Code : " + responseCode);
-	System.out.println("Reason phrase : " + reasonPhrase);
- 
-	BufferedReader rd = new BufferedReader(
-                new InputStreamReader(response.getEntity().getContent()));
- 
-	StringBuffer result = new StringBuffer();
-	String line = "";
-	while ((line = rd.readLine()) != null) {
-		result.append(line);
-	}
- 
-	 System.out.println(result.toString());
-	 
-	 if (responseCode == 302){
-		 return true;
-	 }
-	 else{
-		 return false;
-	 }
- 
-	 
+    // add header
+    post.setHeader("Host", "localhost");
+    post.setHeader("User-Agent", USER_AGENT);
+    post.setHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+    post.setHeader("Cookie", getCookies());
+    post.setHeader("Connection", "keep-alive");
+    post.setHeader("Referer", "http://localhost/classes/access_user/login.php");
+    post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    post.setEntity(new UrlEncodedFormEntity(postParams));
+
+    HttpResponse response = client.execute(post);
+
+    int responseCode = response.getStatusLine().getStatusCode();
+    String reasonPhrase = response.getStatusLine().getReasonPhrase();
+
+    System.out.println("\nSending 'POST' request to URL : " + url);
+    System.out.println("Post parameters : " + postParams);
+    System.out.println("Response Code : " + responseCode);
+    System.out.println("Reason phrase : " + reasonPhrase);
+
+    BufferedReader rd = new BufferedReader(
+        new InputStreamReader(response.getEntity().getContent()));
+
+    StringBuffer result = new StringBuffer();
+    String line = "";
+    while ((line = rd.readLine()) != null) {
+      result.append(line);
+    }
+
+    System.out.println(result.toString());
+
+    if (responseCode == 302){
+      return true;
+    } else{
+      return false;
+    }
   }
- 
+
   private String GetPageContent(String url) throws Exception {
- 
-	HttpGet request = new HttpGet(url);
- 
-	request.setHeader("User-Agent", USER_AGENT);
-	request.setHeader("Accept",
-		"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-	request.setHeader("Accept-Language", "en-US,en;q=0.5");
- 
-	HttpResponse response = client.execute(request);
-	int responseCode = response.getStatusLine().getStatusCode();
- 
-	System.out.println("\nSending 'GET' request to URL : " + url);
-	System.out.println("Response Code : " + responseCode);
- 
-	BufferedReader rd = new BufferedReader(
-                new InputStreamReader(response.getEntity().getContent()));
- 
-	StringBuffer result = new StringBuffer();
-	String line = "";
-	while ((line = rd.readLine()) != null) {
-		result.append(line);
-	}
- 
-	// set cookies
-	setCookies(response.getFirstHeader("Set-Cookie") == null ? "" : 
-                     response.getFirstHeader("Set-Cookie").toString());
- 
-	return result.toString();
- 
+    HttpGet request = new HttpGet(url);
+
+    request.setHeader("User-Agent", USER_AGENT);
+    request.setHeader("Accept",
+        "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+    request.setHeader("Accept-Language", "en-US,en;q=0.5");
+
+    HttpResponse response = client.execute(request);
+    int responseCode = response.getStatusLine().getStatusCode();
+
+    System.out.println("\nSending 'GET' request to URL : " + url);
+    System.out.println("Response Code : " + responseCode);
+
+    BufferedReader rd = new BufferedReader(
+        new InputStreamReader(response.getEntity().getContent()));
+
+    StringBuffer result = new StringBuffer();
+    String line = "";
+    while ((line = rd.readLine()) != null) {
+      result.append(line);
+    }
+
+    // set cookies
+    setCookies(response.getFirstHeader("Set-Cookie") == null ? "" : 
+      response.getFirstHeader("Set-Cookie").toString());
+
+    return result.toString();
   }
- 
+
   public List<NameValuePair> getFormParams(
-             String html, String username, String password)
-			throws UnsupportedEncodingException {
- 
-	System.out.println("Extracting form's data...");
- 
-	Document doc = Jsoup.parse(html);
- 
-	// Google form id
-	//Element loginform = doc.select("form1").first();
-	Element loginform = doc.getElementsByTag("form").first();
-	Elements inputElements = loginform.getElementsByTag("input");
- 
-	List<NameValuePair> paramList = new ArrayList<NameValuePair>();
- 
-	for (Element inputElement : inputElements) {
-		String key = inputElement.attr("name");
-		String value = inputElement.attr("value");
- 
-		if (key.equals("login"))
-			value = username;
-		else if (key.equals("password"))
-			value = password;
- 
-		paramList.add(new BasicNameValuePair(key, value));
- 
-	}
- 
-	return paramList;
+      String html, String username, String password)
+          throws UnsupportedEncodingException {
+
+    System.out.println("Extracting form's data...");
+
+    Document doc = Jsoup.parse(html);
+
+    // Google form id
+    //Element loginform = doc.select("form1").first();
+    Element loginform = doc.getElementsByTag("form").first();
+    Elements inputElements = loginform.getElementsByTag("input");
+
+    List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+
+    for (Element inputElement : inputElements) {
+      String key = inputElement.attr("name");
+      String value = inputElement.attr("value");
+
+      if (key.equals("login"))
+        value = username;
+      else if (key.equals("password"))
+        value = password;
+
+      paramList.add(new BasicNameValuePair(key, value));
+    }
+
+    return paramList;
   }
-  
+
   public List<NameValuePair> getFormParams(
-          String html, String username, String password, String db, String var1, String var2, String ranges,String history)
-			throws UnsupportedEncodingException {
+      String html, String username, String password, String db, String var1, String var2, String ranges,String history)
+          throws UnsupportedEncodingException {
 
-	System.out.println("Extracting form's data...");
+    System.out.println("Extracting form's data...");
 
-	Document doc = Jsoup.parse(html);
+    Document doc = Jsoup.parse(html);
 
-	// Google form id
-	//Element loginform = doc.select("form1").first();
-	Element loginform = doc.getElementsByTag("form").first();
-	Elements inputElements = loginform.getElementsByTag("input");
+    // Google form id
+    //Element loginform = doc.select("form1").first();
+    Element loginform = doc.getElementsByTag("form").first();
+    Elements inputElements = loginform.getElementsByTag("input");
 
-	List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+    List<NameValuePair> paramList = new ArrayList<NameValuePair>();
 
-	for (Element inputElement : inputElements) {
-		String key = inputElement.attr("name");
-		String value = inputElement.attr("value");
+    for (Element inputElement : inputElements) {
+      String key = inputElement.attr("name");
+      String value = inputElement.attr("value");
 
-		if (key.equals("login"))
-			value = username;
-		else if (key.equals("password"))
-			value = password;
-		else if (key.equals("db"))
-			value = db;
-		else if (key.equals("var1"))
-			value = var1;
-		else if (key.equals("var2"))
-			value = var2;
-		else if (key.equals("ranges"))
-			value = ranges;
-		else if (key.equals("history"))
-		  value = history;
+      if (key.equals("login"))
+        value = username;
+      else if (key.equals("password"))
+        value = password;
+      else if (key.equals("db"))
+        value = db;
+      else if (key.equals("var1"))
+        value = var1;
+      else if (key.equals("var2"))
+        value = var2;
+      else if (key.equals("ranges"))
+        value = ranges;
+      else if (key.equals("history"))
+        value = history;
 
-		paramList.add(new BasicNameValuePair(key, value));
+      paramList.add(new BasicNameValuePair(key, value));
+    }
 
-	}
+    return paramList;
+  }
 
-	return paramList;
-}
- 
   public String getCookies() {
-	return cookies;
+    return cookies;
   }
- 
+
   public void setCookies(String cookies) {
-	this.cookies = cookies;
+    this.cookies = cookies;
   }
- 
 }
